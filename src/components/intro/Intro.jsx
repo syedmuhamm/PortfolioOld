0bca88deaf3aad5a094c00fbe0a7925445fcd4fc
syedmuhammadzulqarnain2@gmail.com <syedmuhammzulqarnain2@gmail.com>
2021-10-18 08:05:53 +0200

    Main character picture and social media app pic updated

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -19,7 +19,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/man.png" alt="" />
+          <img src="assets/man.JPG" alt="" />
         </div>
       </div>
       <div className="right">
